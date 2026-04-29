d44e22f65d880dd74d95697d0b3f419abbcef45b
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2026-04-29 17:32:35 +0100

    Adding providers to module

--- variables.tf
+++ variables.tf
@@ -9,3 +9,28 @@ variable "cluster_endpoint" {
   description = "The API endpoint of the cluster"
   type = string
 }
+
+variable "cluster_ca_certificate" {
+  default = ""
+  description = "The CA certificate of the cluster"
+}
+
+variable "provider_endpoint" {
+  default = ""
+  description = "The API endpoint of the cluster for the Kubernetes provider"
+}
+
+variable "provider_cluster_ca_certificate" {
+  default = ""
+  description = "The CA certificate of the cluster for the Kubernetes provider"
+}
+
+variable "provider_cluster_name" {
+  default = ""
+  description = "The name of the cluster for the Kubernetes provider"
+}
+
+variable "provider_token" {
+  default = ""
+  description = "The token for authenticating to the cluster for the Kubectl provider"
+}
