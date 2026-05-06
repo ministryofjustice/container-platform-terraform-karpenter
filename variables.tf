e99d08671bafef6d70852f013a4f4b14b02284f2
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2026-05-06 10:08:15 +0100

    Removing old vars and adding cluster version for alias command

--- variables.tf
+++ variables.tf
@@ -10,27 +10,8 @@ variable "cluster_endpoint" {
   type = string
 }
 
-variable "cluster_ca_certificate" {
-  default = ""
-  description = "The CA certificate of the cluster"
-}
-
-variable "provider_endpoint" {
-  default = ""
-  description = "The API endpoint of the cluster for the Kubernetes provider"
-}
-
-variable "provider_cluster_ca_certificate" {
-  default = ""
-  description = "The CA certificate of the cluster for the Kubernetes provider"
-}
-
-variable "provider_cluster_name" {
-  default = ""
-  description = "The name of the cluster for the Kubernetes provider"
-}
-
-variable "provider_token" {
-  default = ""
-  description = "The token for authenticating to the cluster for the Kubectl provider"
+variable "k8s_version" {
+    default = ""
+    description = "The Kubernetes version of the cluster, e.g. 1.35"
+    type = string
 }
